0e92420df64c7270c5e99754b2168dd2cdf25440
stacksantos <stacksantos@proton.me>
2022-08-21 16:30:35 -0500

    add card number format with spaces between. add max and min length to all input fields. add required to all input fields.

--- app.js
+++ app.js
@@ -12,43 +12,75 @@ const cvc = document.getElementById('cvc');
 const backInfo = document.getElementById('backInfo');
 const input = document.querySelectorAll('input');
 
-setInterval(function checkInput() {
-  input.forEach((input) => {
-    input.addEventListener('keyup', () => {
-      // cardholder change
-      if (input === cardHolder) {
-        cardName.innerHTML = cardHolder.value;
-        if (cardHolder.value === '') {
-          cardName.innerHTML = 'Jane Appleseed';
-        }
+// front number sets
+const nSet1 = document.getElementById('nSet1');
+const nSet2 = document.getElementById('nSet2');
+const nSet3 = document.getElementById('nSet3');
+const nSet4 = document.getElementById('nSet4');
+
+let n1 = '0000';
+let n2 = '0000';
+let n3 = '0000';
+let n4 = '0000';
+
+input.forEach((input) => {
+  input.addEventListener('keyup', () => {
+    // cardholder change
+    if (input === cardHolder) {
+      cardName.innerHTML = cardHolder.value;
+      if (cardHolder.value === '') {
+        cardName.innerHTML = 'Jane Appleseed';
       }
-      // cardnumber change
-      if (input === cardNumber) {
-        frontNumbers.innerHTML = cardNumber.value;
-        if (cardNumber.value === '') {
-          frontNumbers.innerHTML = '0000 0000 0000 0000';
-        }
+    }
+    // cardnumber change
+    if (input === cardNumber) {
+      if (cardNumber.value.length <= 4) {
+        n1 = cardNumber.value;
+        nSet1.innerHTML = n1;
+      } else if (cardNumber.value.length > 4 && cardNumber.value.length <= 8) {
+        n2 = cardNumber.value.slice(4);
+        nSet2.innerHTML = n2;
+      } else if (cardNumber.value.length > 8 && cardNumber.value.length <= 12) {
+        n3 = cardNumber.value.slice(8);
+        nSet3.innerHTML = n3;
+      } else if (cardNumber.value.length > 12) {
+        n4 = cardNumber.value.slice(12);
+        nSet4.innerHTML = n4;
       }
-      // expdate change
-      if (input === month) {
-        expoMonth.innerHTML = month.value;
-        if (month.value === '') {
-          expoMonth.innerHTML = '00';
-        }
+      if (cardNumber.value.length < 1) {
+        n1 = '0000';
+        nSet1.innerHTML = n1;
+      } else if (cardNumber.value.length <= 4) {
+        n2 = '0000';
+        nSet2.innerHTML = n2;
+      } else if (cardNumber.value.length <= 8) {
+        n3 = '0000';
+        nSet3.innerHTML = n3;
+      } else if (cardNumber.value.length <= 12) {
+        n4 = '0000';
+        nSet4.innerHTML = n4;
       }
-      if (input === year) {
-        expoYear.innerHTML = year.value;
-        if (year.value === '') {
-          expoYear.innerHTML = '00';
-        }
+    }
+    // expdate change
+    if (input === month) {
+      expoMonth.innerHTML = month.value;
+      if (month.value === '') {
+        expoMonth.innerHTML = '00';
       }
-      //cvc change
-      if (input === cvc) {
-        backInfo.innerHTML = cvc.value;
-        if (cvc === "") {
-          backInfo.innerHTML = "000"
-        }
+    }
+    if (input === year) {
+      expoYear.innerHTML = year.value;
+      if (year.value === '') {
+        expoYear.innerHTML = '00';
       }
-    });
+    }
+    //cvc change
+    if (input === cvc) {
+      backInfo.innerHTML = cvc.value;
+      if (cvc.value === '') {
+        backInfo.innerHTML = '000';
+      }
+    }
   });
-}, 1);
+});
+
